fcaa3feb8e3dd035202d81b351482fcdbd3198c3
Noah Wilson <nrwilson2@ualr.edu>
2022-10-10 22:45:05 -0500

    Assignment Completed

---
+++ a03-android-resources-master/app/src/main/java/com/ualr/resources/MainActivity.java
@@ -0,0 +1,37 @@
+package com.ualr.resources;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+// TODO 3: Define the alternative resources needed to have different content and look n feel depending on the device language.
+// TODO 4: Get the description string value from resources
+// TODO 5: Initialize the text property of the TextView element with the "country_description_text" id by using the value retrieved in the previous TODO point
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView view = findViewById(R.id.country_description_text);
+        view.setText(R.string.country_description);
+    }
+
+    public void onLearnBtnClicked(View view) {
+        Snackbar.make(view, R.string.congrats_text, BaseTransientBottomBar.LENGTH_SHORT).show();
+    }
+
+}
